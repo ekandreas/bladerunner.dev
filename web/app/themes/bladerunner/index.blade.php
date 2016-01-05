7b8756bdf136aa61fefa195519a96d11c257124a
Andreas Ek <andreas@andreasek.se>
2016-01-06 00:49:32 +0100

    Theme updated with content section

--- web/app/themes/bladerunner/index.blade.php
+++ web/app/themes/bladerunner/index.blade.php
@@ -1,1 +1,30 @@
 @extends('views.master')
+
+@section('content')
+
+    <div class="title">Bladerunner</div>
+    <p>
+    	Page rendered: {{ date('Y-m-d H:i:s') }} GMT
+    </p>
+    <p>
+    	<a onclick="ga( 'send', 'event', 'Downloads', 'dist', '{{ date("Y-m-d H:i", @filectime( $_SERVER['DOCUMENT_ROOT'] . '/bladerunner.zip') ) }}');" href="/bladerunner.zip">
+    		<strong>(+) Download dist bladrunner.zip, created {{ date("Y-m-d H:i", @filectime( $_SERVER['DOCUMENT_ROOT'] . '/bladerunner.zip') ) }} GMT</strong>
+    	</a>
+    </p>
+    <p>
+    	<a onclick="ga( 'send', 'event', 'Outbound', 'packagist', '{{ date("Y-m-d") }}' );" href="https://packagist.org/packages/ekandreas/bladerunner" target="_blank">
+    		Packagist
+    	</a>
+    </p>
+    <p>
+    	<a onclick="ga( 'send', 'event', 'Outbound', 'support', '{{ date("Y-m-d") }}' );" href="https://github.com/ekandreas/bladerunner/issues" target="_blank">
+    		Support
+    	</a>
+    </p>
+    <p>
+    	<a onclick="ga( 'send', 'event', 'Outbound', 'github', '{{ date("Y-m-d") }}' );" href="https://github.com/ekandreas/bladerunner" target="_blank">
+    		Github
+    	</a>
+    </p>
+
+@endsection
--- web/app/themes/bladerunner/views/master.blade.php
+++ web/app/themes/bladerunner/views/master.blade.php
@@ -39,30 +39,7 @@
     	@include( 'views.parts.ga' )
         <div class="container">
             <div class="content">
-                <div class="title">Bladerunner</div>
-                <p>
-                	Page rendered: {{ date('Y-m-d H:i') }} GMT
-                </p>
-                <p>
-                	<a onclick="ga( 'send', 'event', 'Downloads', 'dist', '{{ date("Y-m-d H:i", @filectime( $_SERVER['DOCUMENT_ROOT'] . '/bladerunner.zip') ) }}');" href="/bladerunner.zip">
-                		<strong>(+) Download dist bladrunner.zip, created {{ date("Y-m-d H:i", @filectime( $_SERVER['DOCUMENT_ROOT'] . '/bladerunner.zip') ) }} GMT</strong>
-                	</a>
-                </p>
-                <p>
-                	<a onclick="ga( 'send', 'event', 'Outbound', 'packagist', '{{ date("Y-m-d") }}' );" href="https://packagist.org/packages/ekandreas/bladerunner" target="_blank">
-                		Packagist
-                	</a>
-                </p>
-                <p>
-                	<a onclick="ga( 'send', 'event', 'Outbound', 'support', '{{ date("Y-m-d") }}' );" href="https://github.com/ekandreas/bladerunner/issues" target="_blank">
-                		Support
-                	</a>
-                </p>
-                <p>
-                	<a onclick="ga( 'send', 'event', 'Outbound', 'github', '{{ date("Y-m-d") }}' );" href="https://github.com/ekandreas/bladerunner" target="_blank">
-                		Github
-                	</a>
-                </p>
+            	@yield('content')
             </div>
         </div>
     </body>
