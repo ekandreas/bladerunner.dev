86a1ee902904652b10d48f5cdbd96684c147b6d0
Andreas Ek <andreas@andreasek.se>
2016-01-05 19:56:08 +0100

    Support and packagist links added

--- web/app/themes/bladerunner/views/master.blade.php
+++ web/app/themes/bladerunner/views/master.blade.php
@@ -48,6 +48,21 @@
                 		<strong>(+) Download dist bladrunner.zip, created {{ date("Y-m-d H:i", @filectime( $_SERVER['DOCUMENT_ROOT'] . '/bladerunner.zip') ) }}</strong>
                 	</a>
                 </p>
+                <p>
+                	<a onclick="ga( 'send', 'event', 'Outbound', 'packagist', '{{ date("Y-m-d") }}' );" href="https://packagist.org/packages/ekandreas/bladerunner" target="_blank">
+                		Packagist
+                	</a>
+                </p>
+                <p>
+                	<a onclick="ga( 'send', 'event', 'Outbound', 'support', '{{ date("Y-m-d") }}' );" href="https://github.com/ekandreas/bladerunner/issues" target="_blank">
+                		Support
+                	</a>
+                </p>
+                <p>
+                	<a onclick="ga( 'send', 'event', 'Outbound', 'github', '{{ date("Y-m-d") }}' );" href="https://github.com/ekandreas/bladerunner" target="_blank">
+                		Github
+                	</a>
+                </p>
             </div>
         </div>
     </body>
