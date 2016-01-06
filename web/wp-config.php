c667803e4398abf61ed6d83258a8750809285c07
Andreas Ek <andreas@andreasek.se>
2016-01-06 21:51:16 +0100

    Adding query monitor and debug whoops

--- web/wp-config.php
+++ web/wp-config.php
@@ -5,5 +5,10 @@
  * WP is hardcoded to look in its own directory or one directory up for wp-config.php.
  */
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
+
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
 require_once(dirname(__DIR__) . '/config/application.php');
 require_once(ABSPATH . 'wp-settings.php');
