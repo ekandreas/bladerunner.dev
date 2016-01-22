e2910bfe4bf4ce3a4cbaa6288bca23ddecc4c4ca
Andreas Ek <andreas@andreasek.se>
2016-01-22 15:42:53 +0100

    Logo updated

--- web/app/themes/bladerunner/views/parts/header.blade.php
+++ web/app/themes/bladerunner/views/parts/header.blade.php
@@ -1,6 +1,8 @@
+<div class="row">
   <a href="/">
     <img src="{{ get_stylesheet_directory_uri() . '/assets/images/bladerunner.png' }}" class="img-responsive" style="width:100%" />
   </a>
+</div>
 <!--nav class="navbar navbar-default">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
