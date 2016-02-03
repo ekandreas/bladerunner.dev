8dd360b23cab003b34ae291c5bd7da4a2bed99ec
Andreas Ek <andreas@andreasek.se>
2016-02-03 10:10:57 +0100

    No shiled in footer

--- web/app/themes/bladerunner/views/parts/footer.blade.php
+++ web/app/themes/bladerunner/views/parts/footer.blade.php
@@ -10,26 +10,9 @@
       </p>
 
       <p>
-        <a onclick="ga( 'send', 'event', 'Downloads', 'dist', '{{ date("Y-m-d H:i", @filectime( $_SERVER['DOCUMENT_ROOT'] . '/bladerunner.zip') ) }}');" href="/bladerunner.zip">
-          {!! shieldHtmlImage('https://img.shields.io/github/release/ekandreas/bladerunner.svg') !!} WordPress plugin (zip)
-        </a>
-      </p>
-
-      <p>
-        <a onclick="ga( 'send', 'event', 'Outbound', 'packagist', '{{ date("Y-m-d") }}' );" href="https://packagist.org/packages/ekandreas/bladerunner" target="_blank">
-          {!! shieldHtmlImage('https://img.shields.io/packagist/v/ekandreas/bladerunner.svg') !!}</a>
-        &nbsp;
-        <a onclick="ga( 'send', 'event', 'Outbound', 'support', '{{ date("Y-m-d") }}' );" href="https://github.com/ekandreas/bladerunner/issues" target="_blank">
-          {!! shieldHtmlImage('https://img.shields.io/github/issues/ekandreas/bladerunner.svg') !!}</a>
-        &nbsp;
-        <a onclick="ga( 'send', 'event', 'Outbound', 'github', '{{ date("Y-m-d") }}' );" href="https://github.com/ekandreas/bladerunner" target="_blank">
-          {!! shieldHtmlImage('https://img.shields.io/github/release/ekandreas/bladerunner.svg') !!}</a>
-      </p>
-
-      <p>
         Made by <a href="http://www.aekab.se" rel="nofollow">AEKAB</a>.
         &nbsp;|&nbsp;
-        Contact at <a href="mailto:andreas@aekab.se">andreas@aekab.se</a>.
+        Contact at <a href="mailto:andreas@aekab.se" rel="nofollow">andreas@aekab.se</a>.
       </p>
 
     </div>
